0b0c2ee45377d6532b6f9d62b5ad873fdd6c54d3
2myaus <twomyaus@gmail.com>
2025-09-09 19:02:47 -0400

    verible version overlay in flake.nix (pls change this back once they update to 0.0.4x)

--- flake.nix
+++ flake.nix
@@ -4,7 +4,54 @@
   };
 
   outputs = {nixpkgs, ...}: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    pkgs =
+      nixpkgs.legacyPackages.x86_64-linux.appendOverlays
+      [
+        # (final: prev: {
+        # verible = prev.verible.overrideAttrs (old: {
+        # GIT_VERSION = "v0.0-4023-gc1271a00";
+        # version = "v0.0.4023";
+        # src = prev.fetchFromGitHub {
+        # owner = "chipsalliance";
+        # repo = "verible";
+        # rev = "v0.0-4023-gc1271a00";
+        # hash = "sha256-N+yjRcVxFI56kP3zq+qFHNXZLTtVnQaVnseZS13YN0s=";
+        # };
+        # });
+        # })
+        (final: prev: {
+          verible = prev.stdenv.mkDerivation rec {
+            pname = "verible";
+            version = "v0.0.4023";
+
+            # GitHub release binary tarball
+            src = prev.fetchurl {
+              url = "https://github.com/chipsalliance/verible/releases/download/v0.0-4023-gc1271a00/verible-v0.0-4023-gc1271a00-linux-static-x86_64.tar.gz";
+              sha256 = "sha256-MMIDhZVvUu+JLLWPf4Fvn5qdw3oEMoSKScbZqjpyqLc=";
+            };
+
+            nativeBuildInputs = [prev.autoPatchelfHook];
+
+            # Required for patching dynamic binaries
+            buildInputs = [prev.stdenv.cc.cc.lib];
+
+            # Unpack the tarball (not a source build)
+            dontBuild = true;
+
+            installPhase = ''
+              mkdir -p $out
+              cp -r * $out/
+            '';
+
+            meta = with prev.lib; {
+              description = "Verible is a suite of SystemVerilog developer tools";
+              homepage = "https://github.com/chipsalliance/verible";
+              license = licenses.asl20;
+              platforms = platforms.linux;
+            };
+          };
+        })
+      ];
   in {
     devShells.x86_64-linux.default = pkgs.mkShell {
       buildInputs = [
