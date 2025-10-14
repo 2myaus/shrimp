34eeb46d6fa1ea73bb8af719560e8a617f896ae5
2myaus <twomyaus@gmail.com>
2025-10-14 16:07:34 -0400

    byte input to memfile

--- cpu/memfile.vhdl
+++ cpu/memfile.vhdl
@@ -14,6 +14,7 @@ entity memfile is
     in_addr_2 : in word; -- second address for reading
     write_val : in word; -- value to write to in_addr
     write_enable : in std_logic; -- whether to update memory address on clock
+    byte_1 : in std_logic; -- read/write only a single byte to addr_1 instead of a full word
     clock : in std_logic;
 
     read_val_1 : out word; -- address currently at in_addr_1
@@ -29,11 +30,17 @@ begin
     variable in_addr_1_int : integer := to_integer(unsigned(in_addr_1));
     variable in_addr_2_int : integer := to_integer(unsigned(in_addr_2));
   begin
-    read_val_1(15 downto 7) <= bytes(in_addr_1_int);
-    read_val_1(7 downto 0) <= bytes(in_addr_1_int+1);
 
-    read_val_2(15 downto 7) <= bytes(in_addr_2_int);
+    read_val_2(15 downto 8) <= bytes(in_addr_2_int);
     read_val_2(7 downto 0) <= bytes(in_addr_2_int+1);
+
+    if byte_1 then
+      read_val_1(15 downto 8) <= (others => '0');
+      read_val_1(7 downto 0) <= bytes(in_addr_1_int);
+    else
+      read_val_1(15 downto 8) <= bytes(in_addr_1_int);
+      read_val_1(7 downto 0) <= bytes(in_addr_1_int+1);
+    end if;
   end process;
 
   process(clock) is
@@ -41,7 +48,12 @@ begin
   begin
     if rising_edge(clock) then
       if write_enable then
-        bytes(in_addr_int) <= write_val;
+        if byte_1 then
+          bytes(in_addr_int) <= write_val(15 downto 8);
+          bytes(in_addr_int + 1) <= write_val(7 downto 0);
+        else
+          bytes(in_addr_int) <= write_val(7 downto 0);
+        end if;
       end if;
     end if;
   end process;
